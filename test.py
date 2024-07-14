bf1931bf44b8ef23095a50a64d6e2a80f3f34448
Mark Harrison <mark.z.harrison@gmail.com>
2024-07-14 06:49:15 -0700

    Test no-user-folder error message
    Part of #52

--- test.py
+++ test.py
@@ -914,6 +914,12 @@ Debug:""")
 class ErrorTest(unittest.TestCase):
     """Test that bad user inputs raise correct exceptions."""
 
+    def test_user_folder_errors(self) -> None:
+        """Test that omitting the user folder prints the correct error message."""
+        with self.assertLogs(vintagebackup.logger, logging.ERROR) as log_check:
+            vintagebackup.main(["-l", os.devnull])
+            self.assertEqual(log_check.output, ["ERROR:vintagebackup:User's folder not specified."])
+
     def test_backup_errors(self) -> None:
         """Test that omitting the backup folder prints the correct error message."""
         with (tempfile.TemporaryDirectory() as user_folder,
