a89a3293a1e8ea3d20a5ce38411c9a74b52cb5f7
Mark Harrison <MarkZH@users.noreply.github.com>
2025-08-02 03:15:39 -0700

    Delete ConcurrencyError
    This belongs with Backup_Lock.

--- lib/exceptions.py
+++ lib/exceptions.py
@@ -1,7 +1,3 @@
 
 class CommandLineError(ValueError):
     """An exception class to catch invalid command line parameters."""
-
-
-class ConcurrencyError(RuntimeError):
-    """An exception thrown when another process is using the same backup location."""
