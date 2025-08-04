6c34296de5af56f249e99ebf2a655f33b953b5b7
Mark Harrison <MarkZH@users.noreply.github.com>
2025-08-04 00:08:20 -0700

    Create logger for entry point

--- vintagebackup.py
+++ vintagebackup.py
@@ -7,6 +7,7 @@ from lib.main import main
 
 if __name__ == "__main__":
     try:
+        logger = logging.getLogger(__name__)
         logger.addHandler(logging.StreamHandler(sys.stdout))
         sys.exit(main(sys.argv))
     except KeyboardInterrupt:
