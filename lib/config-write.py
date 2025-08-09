e3c81090cfd1739f152a110024427389d53b4ebe
Mark Harrison <MarkZH@users.noreply.github.com>
2025-08-08 22:34:53 -0700

    Delete generate_windows_scripts from config-write

--- lib/config-write.py
+++ lib/config-write.py
@@ -3,7 +3,6 @@
 import logging
 import os
 import argparse
-import sys
 from pathlib import Path
 from typing import Any
 
@@ -43,25 +42,3 @@ def generate_config(args: argparse.Namespace) -> Path:
 
     logger.info("Generated configuration file: %s", config_path)
     return config_path
-
-
-def generate_windows_scripts(args: argparse.Namespace) -> None:
-    """Generate files for use with Windows Task Scheduler."""
-    destination = absolute_path(args.generate_windows_scripts)
-    args.generate_config = str(destination/"config.txt")
-    config_path = generate_config(args)
-
-    batch_file = unique_path_name(destination/"batch_script.bat")
-    script_location = Path(__file__).parent/"vintagebackup.py"
-    python_version = f"{sys.version_info[0]}.{sys.version_info[1]}"
-    batch_file.write_text(f'py -{python_version} "{script_location}" --config "{config_path}"\n')
-    logger.info("Generated batch script: %s", batch_file)
-
-    vb_script_file = unique_path_name(destination/"vb_script.vbs")
-    vb_script_file.write_text(
-f'''Dim Shell
-Set Shell = CreateObject("WScript.Shell")
-Shell.Run """{batch_file}""", 0, true
-Set Shell = Nothing
-''')
-    logger.info("Generated VB script: %s", vb_script_file)
