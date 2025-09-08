5c289e5cf75c3cd72dd44e8dcf2bd9fa2d59b01e
Mark Harrison <MarkZH@users.noreply.github.com>
2025-09-08 00:10:34 -0700

    Add tests for search_backups()
    Since this function is wrapped by choose_target_from_backups(), the
    tests are copied and adapted from there.

--- testing/test.py
+++ testing/test.py
@@ -1013,6 +1013,72 @@ class RecoveryTests(TestCaseWithTemporaryFilesAndFolders):
             logs.output[-1],
             f"INFO:root:No backups found for the folder {bad_directory}")
 
+    def test_search_backups_finds_all_user_files_in_backup(self) -> None:
+        """The function search_backups() can find all user files after backups."""
+        create_user_data(self.user_path)
+        default_backup(self.user_path, self.backup_path)
+        for current_directory, directories, files in self.user_path.walk():
+            all_paths = sorted(itertools.chain(files, directories))
+            for choice, path in enumerate(all_paths, 0):
+                found_path = recovery.search_backups(
+                    current_directory,
+                    self.backup_path,
+                    "",
+                    choice)
+                self.assertEqual(current_directory/path, found_path)
+
+    def test_search_backups_finds_added_user_files(self) -> None:
+        """The function search_backups() can find all user files after backups."""
+        create_user_data(self.user_path)
+        default_backup(self.user_path, self.backup_path)
+        (self.user_path/"extra_file.txt").touch(exist_ok=False)
+        default_backup(self.user_path, self.backup_path)
+        for current_directory, directories, files in self.user_path.walk():
+            all_paths = sorted(itertools.chain(files, directories))
+            for choice, path in enumerate(all_paths, 0):
+                found_path = recovery.search_backups(
+                    current_directory,
+                    self.backup_path,
+                    "",
+                    choice)
+                self.assertEqual(current_directory/path, found_path)
+
+    def test_search_backups_raises_error_for_non_existant_folder(self) -> None:
+        """If a search directory does not exist, the function raises a CommandLineException."""
+        create_user_data(self.user_path)
+        default_backup(self.user_path, self.backup_path)
+        bad_directory = self.user_path/"does-not-exist"
+        with self.assertRaises(CommandLineError) as error:
+            recovery.search_backups(bad_directory, self.backup_path, "")
+        self.assertEqual(
+            error.exception.args[0],
+            f"The given search path is not a directory: {bad_directory}")
+
+    def test_search_backups_raises_error_for_non_folder(self) -> None:
+        """If a search directory is not a directory, the function raises a CommandLineException."""
+        create_user_data(self.user_path)
+        default_backup(self.user_path, self.backup_path)
+        bad_directory = self.user_path/"does-not-exist"
+        bad_directory.touch()
+        with self.assertRaises(CommandLineError) as error:
+            recovery.search_backups(bad_directory, self.backup_path, "")
+        self.assertEqual(
+            error.exception.args[0],
+            f"The given search path is not a directory: {bad_directory}")
+
+    def test_search_backups_returns_none_and_logs_for_empty_folder(self) -> None:
+        """If nothing is found for a search directory, the function returns None and logs."""
+        create_user_data(self.user_path)
+        bad_directory = self.user_path/"does-not-exist"
+        bad_directory.mkdir()
+        default_backup(self.user_path, self.backup_path)
+        with self.assertLogs(level=logging.INFO) as logs:
+            found_path = recovery.search_backups(bad_directory, self.backup_path, "")
+        self.assertIsNone(found_path)
+        self.assertEqual(
+            logs.output[-1],
+            f"INFO:root:No backups found for the folder {bad_directory}")
+
 
 def create_large_files(base_folder: Path, file_size: int) -> None:
     """Create a file of a give size in every leaf subdirectory."""
