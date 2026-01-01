9c2a0c906bfe7cdefab6224374e55779cc922a7b
Mark Harrison <MarkZH@users.noreply.github.com>
2026-01-01 15:01:55 -0800

    Combine import lines

--- lib/restoration.py
+++ lib/restoration.py
@@ -6,9 +6,8 @@ import argparse
 from pathlib import Path
 
 from lib.argument_parser import confirm_choice_made
-from lib.backup_utilities import all_backups
+from lib.backup_utilities import all_backups, find_previous_backup
 from lib.backup_info import backup_source
-from lib.backup_utilities import find_previous_backup
 from lib.console import cancel_key, choose_from_menu, print_run_title
 from lib.exceptions import CommandLineError
 from lib.filesystem import absolute_path, delete_path, get_existing_path
