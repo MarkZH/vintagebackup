fff018113c7816d4839a7064415f90abac1a8df5
Mark Harrison <MarkZH@users.noreply.github.com>
2026-01-20 20:29:22 -0800

    Count size of copied files during backup
    Instead of finding the difference between the backup media free space before and after a backup.
    
    Closes #179

--- testing/test.py
+++ testing/test.py
@@ -581,9 +581,8 @@ class BackupTests(TestCaseWithTemporaryFilesAndFolders):
         with self.assertLogs(level=logging.INFO) as logs:
             exit_code = main_no_log(arguments)
         self.assertEqual(exit_code, 0)
-        expected_message = re.compile(
-            r"INFO:root:Backup space used: 50\.\d\d MB \(51% of --free-up\)")
-        self.assertTrue(any(expected_message.fullmatch(line) for line in logs.output), logs.output)
+        expected_message = "INFO:root:Backup space used: 50.00 MB (50% of --free-up)"
+        self.assertIn(expected_message, logs.output)
         self.assertFalse(any(line.startswith("WARNING:") for line in logs.output), logs.output)
         self.assertFalse(any(line.startswith("ERROR:") for line in logs.output), logs.output)
 
