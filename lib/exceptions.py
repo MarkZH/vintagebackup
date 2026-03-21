5f3ee0b01779d3e6459ecc0aff8f4e14dd20f2e4
Mark Harrison <MarkZH@users.noreply.github.com>
2026-03-21 00:33:31 -0700

    Use correct Python terminology

--- lib/exceptions.py
+++ lib/exceptions.py
@@ -2,15 +2,15 @@
 
 
 class CommandLineError(ValueError):
-    """An exception class to catch invalid command line parameters."""
+    """An exception raised when invalid command line parameters are used."""
 
 
 class ConcurrencyError(RuntimeError):
-    """An exception thrown when another process is using the same backup location."""
+    """An exception raised when another process is using the same backup location."""
 
 
 class OutOfSpaceError(RuntimeError):
-    """An exception thrown when the backup media does not have enough space for the operation."""
+    """An exception raised when the backup media does not have enough space for the operation."""
 
 
 class FilterFileError(ValueError):
